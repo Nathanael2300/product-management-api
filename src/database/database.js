99365ecbea69b053af53f54514d82b24b3d666c5
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-05-17 00:37:55 -0300

    feat: create products table schema

--- src/database/database.js
+++ src/database/database.js
@@ -1,4 +1,5 @@
 import sqlite3 from "sqlite3";
+
 sqlite3.verbose();
 
 const db = new sqlite3.Database("./database.sqlite", (err) => {
@@ -8,6 +9,18 @@ const db = new sqlite3.Database("./database.sqlite", (err) => {
   }
 
   console.log("Connected to SQLite database.");
+});
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS products (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      price REAL NOT NULL,
+      category TEXT,
+      stock INTEGER
+    )
+  `);
 });
 
 export default db;
