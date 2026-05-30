3e83513511735182c8f658f475b81bec3e819d6b
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-05-30 12:31:26 -0300

    feat: Add doteenv in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import("./database/database.js");
 import app from "../src/app.js";
+import dotenv from "dotenv";
 
-const PORT = 3000;
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor runing on port ${PORT}`);
 });
