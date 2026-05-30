cb843c2491384076df9b540a1b5e73b5e0ef8621
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-05-30 00:55:45 -0300

    feat: Add router

---
+++ src/routes/products.routes.js
@@ -0,0 +1,39 @@
+import express from "express";
+import productControllers from "../controllers/product.controllers.js";
+import asyncHandler from "../middlewares/asyncHandler.js";
+import { validate } from "../middlewares/validation.js";
+
+import {
+  getProductById,
+  createProductSchema,
+  updateProductSchema,
+  deleteProductSchema,
+} from "../schema/productSchema.js";
+
+const router = express.Router();
+
+router.get("/", asyncHandler(productControllers.getAllProducts));
+
+router.get(
+  "/:id",
+  validate(getProductById, "params"),
+  asyncHandler(productControllers.getProductById),
+);
+
+router.post(
+  "/",
+  validate(createProductSchema),
+  asyncHandler(productControllers.createProduct),
+);
+
+router.put(
+  "/:id",
+  validate(updateProductSchema),
+  asyncHandler(productControllers.uptateProduct),
+);
+
+router.delete(
+  "/:id",
+  validate(deleteProductSchema, "params"),
+  asyncHandler(productControllers.deleteProduct),
+);
