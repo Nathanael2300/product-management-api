8285d2d8c05cef81ea9a8934d2f48c022e9c24b3
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-06-01 18:40:45 -0300

    feat: Add helpers to ingegraions tests

---
+++ test/helpers/productApi.js
@@ -0,0 +1,26 @@
+import request from "supertest";
+import app from "../../src/app.js";
+
+class UserApi {
+  getAll() {
+    return request(app).get("/products");
+  }
+
+  getById(id) {
+    return request(app).get(`/products/${id}`);
+  }
+
+  create(data) {
+    return request(app).post("/products").send(data);
+  }
+
+  update(id, data) {
+    return request(app).put(`/products/${id}`).send(data);
+  }
+
+  delete(id) {
+    return request(app).delete(`/products/${id}`);
+  }
+}
+
+export default new UserApi();
