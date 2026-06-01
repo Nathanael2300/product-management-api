5f6ae42cf3661c6faa553dc0dc9129c0fc1c7654
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-06-01 18:41:19 -0300

    fix: Update import from productService

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,4 +1,4 @@
-import productService from "../services/productService.js";
+import productService from "../services/product.service.js";
 
 class ProductController {
   async getAllProducts(req, res) {
