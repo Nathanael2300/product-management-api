9456eb922709c94b6daee34f5bc322eb390e6398
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-06-06 00:52:29 -0300

    refactor(app): update app initialization

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import productRouter from "./routes/products.routes.js";
-import errorHandler from "./middlewares/errorMiddleware.js";
+import errorHandler from "./middlewares/error.middleware.js";
 import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
