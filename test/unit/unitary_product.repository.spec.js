4322ab901be3489bd96c44d215989ace695c53b0
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-06-06 00:52:33 -0300

    test(repo): add unit test for product repository

---
+++ test/unit/unitary_product.repository.spec.js
@@ -0,0 +1,154 @@
+import { expect } from "chai";
+import productFactory from "../factory/product.factory.js";
+import productRepository from "../../src/repositories/product.repositories.js";
+import db from "../../src/database/database.js";
+import { faker } from "@faker-js/faker";
+
+describe("Product Model", () => {
+  let productId;
+  beforeEach(async () => {
+    await new Promise((resolve, reject) => {
+      db.run("DELETE FROM products", (err) => {
+        if (err) reject(err);
+        else resolve();
+      });
+    });
+
+    const productData = productFactory();
+    const product = await productRepository.create(productData);
+
+    productId = product.id;
+  });
+
+  describe("/GET/PRODUCTS ", () => {
+    describe("Positive scenarios", () => {
+      it("Should return product list", async () => {
+        const products = await productRepository.getAll();
+
+        const fields = ["id", "name", "price", "category", "stock"];
+        expect(products).to.be.an("array");
+        for (const key of fields) {
+          for (const product of products) {
+            expect(product).to.have.property(key);
+          }
+        }
+      });
+    });
+    describe("Negative scenarios", () => {});
+  });
+
+  describe("/GET/PRODUCTS/:id", () => {
+    describe("Positive scenarios", () => {
+      it("Should return 1 product off product list", async () => {
+        const product = await productRepository.GetById(productId);
+
+        const fields = ["id", "name", "price", "category", "stock"];
+
+        for (const key of fields) {
+          expect(product).to.have.property(key);
+        }
+      });
+    });
+    describe("Negative scenarios", () => {
+      it("should return undefined when product does not exist", async () => {
+        const idInvalid = faker.number.int({ min: 50000 });
+        const product = await productRepository.GetById(idInvalid);
+
+        expect(product).to.eql(undefined);
+      });
+    });
+  });
+
+  describe("/POST/PRODUCTS", () => {
+    describe("Senarios positive", () => {
+      it("Should create a product successfully", async () => {
+        const productData = await productFactory();
+
+        const product = await productRepository.create(productData);
+        const fields = ["id", "name", "price", "category", "stock"];
+        expect(product).to.be.an("object");
+        for (const key of fields) {
+          expect(product).to.have.property(key);
+        }
+      });
+    });
+    describe("Scenarios negative", () => {
+      it("Should not create product when payload is undefined", async () => {
+        try {
+          await productRepository.create(undefined);
+        } catch (err) {
+          expect(err.message).to.equal(
+            "Cannot destructure property 'name' of 'productData' as it is undefined.",
+          );
+        }
+      });
+    });
+  });
+
+  describe("/UPDATE/PRODUCT/:id", () => {
+    describe("Senarios positive", () => {
+      it("Should update a product data sucessfully", async () => {
+        const fields = ["id", "name", "price", "category", "stock"];
+        const productById = await productRepository.GetById(productId);
+        for (const key of fields) {
+          expect(productById).to.have.property(key);
+          expect(productById).to.exist;
+        }
+
+        for (const [key, value] of Object.entries(productById)) {
+          expect(productById).to.have.property(key);
+
+          expect(value).to.exist;
+        }
+        const productUpdatedData = await productFactory();
+        const product = await productRepository.update(
+          productById.id,
+          productUpdatedData,
+        );
+        for (const [key, value] of Object.entries(product)) {
+          expect(product).to.have.property(key);
+
+          expect(value).to.exist;
+        }
+
+        const updatedProduct = await productRepository.GetById(productById.id);
+        for (const [key, value] of Object.entries(updatedProduct)) {
+          expect(updatedProduct).to.have.property(key);
+          expect(value).to.exist;
+          expect(updatedProduct.name).to.equal(productUpdatedData.name);
+          expect(updatedProduct.price).to.equal(productUpdatedData.price);
+          expect(updatedProduct.category).to.equal(productUpdatedData.category);
+          expect(updatedProduct.stock).to.equal(productUpdatedData.stock);
+        }
+      });
+    });
+    describe("Senarios negative", () => {
+      it("Should not update product when payload is undefined", async () => {
+        try {
+          await productRepository.update(productId, undefined);
+        } catch (err) {
+          expect(err.message).to.equal(
+            "Cannot destructure property 'name' of 'productData' as it is undefined.",
+          );
+        }
+      });
+    });
+  });
+
+  describe("/DELETE/:id", () => {
+    describe("Senarios positive", () => {
+      it("Should delete user successfully", async () => {
+        const product = await productRepository.delete(productId);
+        expect(product).to.have.property("deleted", 1);
+      });
+    });
+    describe("Senarios negative", () => {
+      it("Should not delete when user id is invalid", async () => {
+        const idInvalid = faker.number.int({ min: 50000 });
+        const product = await productRepository.delete(idInvalid);
+
+        expect(product).to.have.property("deleted", 0);
+      });
+    });
+  });
+});
