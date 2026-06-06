a9f38a8f7c7404092424aa408caeee430069e957
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-06-06 00:52:31 -0300

    feat(model): define product model schema

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,10 +1,20 @@
 class Product {
-  constructor(id, name, price, category, stock) {
+  constructor({ id, name, price, category, stock }) {
     this.id = id;
     this.name = name;
     this.price = price;
     this.category = category;
     this.stock = stock;
+  }
+
+  validate() {
+    if (this.price < 0) {
+      throw new Error("Price cannot be negative");
+    }
+
+    if (this.stock < 0) {
+      throw new Error("Stock cannot be negative");
+    }
   }
 }
 
