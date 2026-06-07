675bf02345ad22c6b2394d54763220dee067a949
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-06-07 12:54:57 -0300

    test: Add logs to revew database

--- src/database/database.js
+++ src/database/database.js
@@ -13,6 +13,9 @@ const db = new sqlite3.Database(dbPath, (err) => {
   }
 });
 
+console.log("Database path:", dbPath);
+console.log("Database initialized");
+
 db.serialize(() => {
   db.run(`
     CREATE TABLE IF NOT EXISTS products (
