f29e03887bb815c2141e837b2a9155e3c6812d5a
Nathanael Henrique Souza Nunes <nathanhenrique.nunes.souza@gmail.com>
2026-06-10 20:23:02 -0300

    feat: Add task on cypress to crean database

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "cypress";
 import mochawesome from "cypress-mochawesome-reporter/plugin.js";
+import db from "./src/database/database.js";
 
 export default defineConfig({
   reporter: "cypress-mochawesome-reporter",
@@ -20,7 +21,22 @@ export default defineConfig({
 
     setupNodeEvents(on, config) {
       mochawesome(on);
+
+      on("task", {
+        clearDatabase() {
+          return new Promise((resolve, reject) => {
+            db.run("DELETE FROM products", (err) => {
+              if (err) {
+                reject(err);
+              }
+              resolve(null);
+            });
+          });
+        },
+      });
       return config;
     },
+
+    baseUrl: "http://localhost:3003",
   },
 });
